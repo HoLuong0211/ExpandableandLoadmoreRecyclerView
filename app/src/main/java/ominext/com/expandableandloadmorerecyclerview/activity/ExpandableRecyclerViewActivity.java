f823f5cecce15c5bff8f46139feaef6c4d9d6e89
LuongHH <luonghh@ominext.com>
2017-03-16 14:23:47 +0700

    init project

---
+++ app/src/main/java/ominext/com/expandableandloadmorerecyclerview/activity/ExpandableRecyclerViewActivity.java
@@ -0,0 +1,15 @@
+package ominext.com.expandableandloadmorerecyclerview.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ominext.com.expandableandloadmorerecyclerview.R;
+
+public class ExpandableRecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expandable_recycler_view);
+    }
+}
---
+++ app/src/main/java/ominext/com/expandableandloadmorerecyclerview/activity/MainActivity.java
@@ -0,0 +1,24 @@
+package ominext.com.expandableandloadmorerecyclerview.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import ominext.com.expandableandloadmorerecyclerview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void expandableRecyclerView(View v) {
+
+    }
+
+    public void loadMoreRecyclerView(View v) {
+
+    }
+}
